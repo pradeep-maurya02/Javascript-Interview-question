488e77214b52048ba64099b5826841ae551b77a2
pradeep-maurya02 <pradeep6278068@gmail.com>
2025-01-23 16:05:48 +0530

    Filter out the marks of students that scored 90+.
    // We are given array of marks of students. Filter out of the marks of students that scored 90+.

---
+++ Marks.js
@@ -0,0 +1,6 @@
+// for a given arrya of number , print the square of each value using th forEach loop.
+let marks=[87,93,64,99,86];
+let topper = marks.filter((val) => {
+  return val > 90;
+}); 
+console.log(topper);
